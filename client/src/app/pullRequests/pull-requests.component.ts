21527947c2af904dd47a245639f4b6d514d1661a
lenny <valentin805@gmail.com>
2019-07-03 20:42:02 +0300

    Create PullRequestModule.

---
+++ client/src/app/pullRequests/pull-requests.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+// pull-requests.component
+@Component({
+    selector: 'pull-requests',
+    templateUrl: './pull-requests.component.html',
+    styleUrls: ['./pull-requests.component.css']
+  })
+  export class PullRequestsComponent{
+      
+  }
+  
