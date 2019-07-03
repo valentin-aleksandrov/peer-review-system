fd87a1a5efc20f78c880c8c535c14f9c7e85b99f
Stiliana Georgieva <stiliana.yordanova@gmail.com>
2019-07-03 17:36:51 +0300

    merge conflicts resolved

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
