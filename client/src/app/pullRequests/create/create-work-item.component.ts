cb65bd11a5ac3d7dcffbb4796019cfbc008ea41f
lenny <valentin805@gmail.com>
2019-07-16 16:39:16 +0300

    Create a CreateWorkItemComponent.

---
+++ client/src/app/pullRequests/create/create-work-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'create-work-item',
+    templateUrl: './create-work-item.component.html',
+    styleUrls: ['./create-work-item.component.css']
+  })
+  export class CreateWorkItemComponent {
+    title = 'client';
+  }
--- client/src/app/pullRequests/pull-requests.module.ts
+++ client/src/app/pullRequests/pull-requests.module.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { PullRequestsComponent } from './pull-requests.component';
-
+import { CreateWorkItemComponent } from './create/create-work-item.component';
 @NgModule({
     declarations: [
         PullRequestsComponent,
+        CreateWorkItemComponent,
     ],
     imports: [
       SharedModule,
