bf47370422cd9e4011cec658a4994237f5f664a8
lenny <valentin805@gmail.com>
2019-07-25 17:27:45 +0300

    Resolving issue with unsent push notification.

--- api/src/notifications/notifaction.module.ts
+++ api/src/notifications/notifaction.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
 import { EmailService } from "./email.service";
+import { PushNotificationService } from "./push-notification.service";
 
 
 @Module({
-    providers: [EmailService],
-    exports: [EmailService],
+    providers: [
+      EmailService, 
+      PushNotificationService
+    ],
+    exports: [
+      EmailService, 
+      PushNotificationService,
+    ],
   })
   export class NotificatorModule { }
