9fe7c9c643b501e8627e3930acb655e79cd641d8
Stiliana Georgieva <stiliana.yordanova@gmail.com>
2019-07-25 13:23:10 +0300

    workitems table

---
+++ client/src/app/pullRequests/requests-nav/requests-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-requests-nav',
+  templateUrl: './requests-nav.component.html',
+  styleUrls: ['./requests-nav.component.css']
+})
+export class RequestsNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- client/src/app/pullRequests/simpleWorkItem/simple-work-item.component.ts
+++ client/src/app/pullRequests/simpleWorkItem/simple-work-item.component.ts
@@ -1,14 +1,13 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { UserDetails } from 'src/app/models/user-details';
-import { WorkItem } from 'src/app/models/work-item';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { UserDetails } from "src/app/models/user-details";
+import { WorkItem } from "src/app/models/work-item";
 
 @Component({
-    selector: 'simple-work-item',
-    templateUrl: './simple-work-item.component.html',
-    styleUrls: ['./simple-work-item.component.css'],
-  })
-  export class SimpleWorkItemComponent{
-    @Input()
-    workItem :WorkItem;
-
-  }
+  selector: "simple-work-item",
+  templateUrl: "./simple-work-item.component.html",
+  styleUrls: ["./simple-work-item.component.css"]
+})
+export class SimpleWorkItemComponent {
+  @Input()
+  workItem: WorkItem;
+}
