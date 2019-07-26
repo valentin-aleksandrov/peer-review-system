d1012325bb5540936b424bc4f472f0a2422f3869
Stiliana Georgieva <stiliana.yordanova@gmail.com>
2019-07-26 19:21:12 +0300

    auth guard implementation

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,26 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { LoginComponent } from "./components/login/login.component";
+import { AuthGuard } from "./guards/auth-guard";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'pullRequests',
-    loadChildren: './pullRequests/pull-requests.module#PullRequestsModule',
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
+  {
+    path: "pullRequests",
+    canActivate: [AuthGuard],
+    loadChildren: "./pullRequests/pull-requests.module#PullRequestsModule"
     //'./users/users.module#UsersModule',
   },
-  { 
-    path: 'profile',
-    loadChildren: './profile/profile.module#ProfileModule',
-  },
-]
+  {
+    path: "profile",
+    canActivate: [AuthGuard],
+    loadChildren: "./profile/profile.module#ProfileModule"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
