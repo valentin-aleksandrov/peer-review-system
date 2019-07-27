b93e4b7c78d66968a951e8262ea76bc401d07d3c
lenny <valentin805@gmail.com>
2019-07-28 00:16:12 +0300

    Implement notification for review change status.

--- api/src/review-requests/review-requests.service.ts
+++ api/src/review-requests/review-requests.service.ts
@@ -15,6 +15,7 @@ import { ShowReviewDTO } from "./models/show-review.dto";
 import { CombinedReviewDTO } from "./models/combined-review.dto";
 import { EmailService } from "src/notifications/email.service";
 import { PushNotificationService } from "src/notifications/push-notification.service";
+import { ShowUserDTO } from "src/users/models/show-user.dto";
 
 @Injectable()
 export class ReviewRequestsService {
@@ -61,33 +62,85 @@ export class ReviewRequestsService {
     const newStatus: ReviewerStatus = await this.reviewStatusRepository.findOne(
       { where: { status: status.status } },
     );
-    console.log("revId", reviewId);
+
     const review: Review = await this.reviewRepository.findOne({
       where: { id: reviewId },
     });
-    console.log("review", review);
+
     const commentContent: AddCommentDTO = {
       content: status.content,
     };
-    let commentEntity: ShowCommentDTO = null;
+    let commentDTO: ShowCommentDTO = null;
+    
+    
     if (newStatus.status !== "pending") {
-      commentEntity = await this.createReviewRequestComment(
+      commentDTO = await this.createReviewRequestComment(
         workItemId,
         commentContent,
         user,
       );
     }
+    
     review.reviewerStatus = newStatus;
     const newReview = await this.reviewRepository.save(review);
-    const reviewToShow: ShowReviewDTO = plainToClass(ShowReviewDTO, newReview, {
-      excludeExtraneousValues: true,
-    });
+
+    // plain to class returns ShowReview without a reviewStatus and a user D: (linux issue)
+    // const reviewToShow: ShowReviewDTO = plainToClass(ShowReviewDTO, newReview, {
+    //   excludeExtraneousValues: true,
+    // });
+    const reviewToShow: ShowReviewDTO = {
+      id: newReview.id,
+      reviewStatus: newReview.reviewerStatus,
+      user: await this.convertToShowUserDTO(newReview.user),
+    };
+
+    
     const combined: CombinedReviewDTO = {
       review: reviewToShow,
-      comment: commentEntity,
+      comment: commentDTO,
     };
+    const workItem: WorkItem = await this.workItemRepository.findOne({ where: {
+      id: workItemId,
+    }})
+    this.notifyForReviewStatusChange(workItem,combined);
     return await combined;
   }
+  private async notifyForReviewStatusChange(workItem: WorkItem, combined: CombinedReviewDTO): Promise<void> {  
+    const reviews: Review[] = await workItem.reviews;
+    // I need this step, because linux doesn't load reviews with all of data (only IDs D:)
+    let loadedReviews: Review[] = [];
+    for (const currentReview of reviews) {
+      const currentlyLoadedReview: Review = await this.reviewRepository.findOne({
+        where: {
+          id: currentReview.id,
+        }
+      });
+      loadedReviews = [currentlyLoadedReview,...loadedReviews];
+    }
+    this.notifyUserForReviewStatusChange(workItem.author,workItem, combined);  
+
+    loadedReviews
+      .map((review: Review)=>review.user)
+      .filter((user: User)=>user.id !== combined.review.user.id)
+      .forEach((user: User)=>this.notifyUserForReviewStatusChange(user,workItem, combined));
+  }
+  private notifyUserForReviewStatusChange(user: User, workItem: WorkItem, combined: CombinedReviewDTO): void {
+    const link: string = `http://localhost:4200/pullRequests/${workItem.id}`;
+    
+    this.emailService.sendEmail(
+      user.email,
+      'Review status change',
+      `${combined.comment.author.username} change his review status to ${combined.review.reviewStatus.status} for Work Item ${workItem.title} because ${combined.comment.content}. Press here: ${link}`
+    );
+
+    this.pushNotificationService.sendPushNotfication(
+      'Review status change',
+      `${combined.comment.author.username} change his review status to ${combined.review.reviewStatus.status} for Work Item ${workItem.title}. Press here:`,
+      user.username,
+      link
+    );
+  }
+
   private async notifyForWorkItemComment(comment: ShowCommentDTO, workItem: WorkItem): Promise<void> {
     const workItemAuthor: User = workItem.author;
     const reviews: Review[] = await workItem.reviews;
@@ -126,5 +179,17 @@ export class ReviewRequestsService {
       user.username,
       link
     );
+  }
+  private async convertToShowUserDTO(user: User): Promise<ShowUserDTO> {
+    const convertedUser: ShowUserDTO = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      role: (await user.role).name,
+      avatarURL: user.avatarURL,
+    };
+    return convertedUser;
   }
 }
