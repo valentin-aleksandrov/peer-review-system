86b8cbc4f645c6ec45d179db845e6c6f9535d823
lenny <valentin805@gmail.com>
2019-07-29 01:22:55 +0300

    Implement fetching prevous chosen tags for a particular work item in EditItemComponent.

--- client/src/app/pullRequests/edit/edit-item.component.ts
+++ client/src/app/pullRequests/edit/edit-item.component.ts
@@ -9,6 +9,7 @@ import { CommentsDataService } from "src/app/core/services/comments-data.service
 import { Comment } from "src/app/models/comment";
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NgbTypeaheadConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Tag } from 'src/app/models/tag';
 
 @Component({
   selector: "edit-item",
@@ -19,6 +20,10 @@ import { NgbTypeaheadConfig } from '@ng-bootstrap/ng-bootstrap';
 export class EditItem implements OnInit {
   public workItem: WorkItem;
   public updateWorkItemForm: FormGroup;
+  public tags: Tag[] = [];
+  
+  public selectedItems: Tag[] = [];
+  public dropdownSettings = {};
   constructor(
     private readonly activatedRoute: ActivatedRoute,
     private readonly formBuilder: FormBuilder,
@@ -27,13 +32,26 @@ export class EditItem implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.data.subscribe((data)=>{
       this.workItem = data.workItem;
+      this.tags = data.tags;
+      this.selectedItems = this.workItem.tags;
     });
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: "id",
+      textField: "name",
+      selectAllText: "Select All",
+      unSelectAllText: "UnSelect All",
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
     console.log(this.workItem);
     this.updateWorkItemForm = this.formBuilder.group({
       title: [this.workItem.title, [Validators.required, Validators.minLength(3)]],
       reviwer: ["", []],
-      tagControl: ["", []],
+      tagControl: [this.workItem.tags, []],
       editorModel: [this.workItem.description,[Validators.required, Validators.minLength(17)]]
     });
   }
+  onItemSelect(item: any) {}
+  onSelectAll(items: any) {}
 }
