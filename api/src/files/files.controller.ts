adfbafbed312597f39d315bc8cd515844bb0caaa
lenny <valentin805@gmail.com>
2019-07-30 16:04:48 +0300

    Successful files using Postman as a client.

--- api/src/files/files.controller.ts
+++ api/src/files/files.controller.ts
@@ -11,18 +11,31 @@ import {
   NotFoundException,
   Query,
   Put,
+  UploadedFiles,
+  UseInterceptors,
 } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { WorkItemService } from "src/workItem/work-item.service";
 import { FilesService } from "./files.service";
+import { FilesInterceptor } from "@nestjs/platform-express";
 
 
 @UseGuards(AuthGuard())
-@Controller("api/work-item")
+@Controller("api/files")
 export class FilesController {
   constructor(
     private readonly workItemService: WorkItemService,
     private readonly filesService: FilesService,
     ) {}
 
+  @Post('workItem/:id')
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadFile(
+    @UploadedFiles() files,
+    @Param("id") workItemId: string,
+    ) {
+    console.log('files',files);
+    console.log('workItemId',workItemId);
+    
+  }
 }
