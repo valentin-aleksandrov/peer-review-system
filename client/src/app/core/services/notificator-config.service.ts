46973e17b2d8f4226f930fe3ce360382faec112a
lenny <valentin805@gmail.com>
2019-07-31 17:29:58 +0300

    Remove error on sign-out.

--- client/src/app/core/services/notificator-config.service.ts
+++ client/src/app/core/services/notificator-config.service.ts
@@ -20,8 +20,13 @@ import { AuthenticationService } from './authentication.service';
              for(let i = 0; i < scripts.length; i++){
                if(scripts[i].innerHTML.toString().includes('window.Engagespot')){
                  console.log('found.');
-                 const currentUserName = this.authenticationService.currentUserValue.user.username;
-                 scripts[i].innerHTML = `window.Engagespot={},q=function(e){return function(){(window.engageq=window.engageq||[]).push({f:e,a:arguments})}},f=["captureEvent","subscribe","init","showPrompt","identifyUser","clearUser"];for(k in f)Engagespot[f[k]]=q(f[k]);var s=document.createElement("script");s.type="text/javascript",s.async=!0,s.src="https://cdn.engagespot.co/EngagespotSDK.2.0.js";var x=document.getElementsByTagName("script")[0];x.parentNode.insertBefore(s,x); Engagespot.init('zmE3Keawnraz8VGfNP2MNFcNxiw7FG');Engagespot.identifyUser('${currentUserName}');`;
+                 if(!this.authenticationService.currentUserValue){
+                  scripts[i].innerHTML = '';
+                 } else {
+                  const currentUserName = this.authenticationService.currentUserValue.user.username;
+                  scripts[i].innerHTML = `window.Engagespot={},q=function(e){return function(){(window.engageq=window.engageq||[]).push({f:e,a:arguments})}},f=["captureEvent","subscribe","init","showPrompt","identifyUser","clearUser"];for(k in f)Engagespot[f[k]]=q(f[k]);var s=document.createElement("script");s.type="text/javascript",s.async=!0,s.src="https://cdn.engagespot.co/EngagespotSDK.2.0.js";var x=document.getElementsByTagName("script")[0];x.parentNode.insertBefore(s,x); Engagespot.init('zmE3Keawnraz8VGfNP2MNFcNxiw7FG');Engagespot.identifyUser('${currentUserName}');`;
+                 }
+                 
                }
              }
            }
